417f778e3efbf2eda8ae78b186098dfc2ca8dbe7
sergeich5w <75609315+sergeich5w@users.noreply.github.com>
2020-12-10 12:50:20 +0300

    parsing name, rating, comment

--- crawl.py
+++ crawl.py
@@ -14,9 +14,9 @@ title = driver.find_element_by_xpath('//h1/span').text
 
 print(title)
 
-time.sleep(1)
+time.sleep(2)
 flag=0
-while 0:
+while 1:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     try:
         loadMore=driver.find_element_by_xpath("//*[contains(@class,'U26fgb O0WRkf oG5Srb C0oVfc n9lfJ')]").click()
@@ -38,21 +38,24 @@ print("There are "+str(len(reviews))+" reviews avaliable")
 
 with open(outputFileName+'.csv', mode='w') as file:
     writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["name","ratings","date","helpful vote","comment"])
+    writer.writerow(["name","ratings","comment"])
     for review in reviews:
-        #try:
-        r = review.find_element_by_css_selector('div:nth-child(1) > div:nth-child(2)')
-        #print r.get_attribute("innerHTML")
-        
-        #name = r.find_element_by_css_selector('div:nth-child(1) > div:nth-child(1) > span').text
-        #print name
-        
-        #comment = r.find_element_by_css_selector('div:nth-child(2)').get_attribute("outerHTML")
-        #print comment
-            #writer.writerow([name.encode('utf-8'),ratings,date,helpful,comment.encode('utf-8')])
-        #except:
-            #print("error")
-        driver.quit()
-        exit()
+        try:
+            r = review.find_element_by_css_selector('div:first-of-type > div:nth-child(2)')
+            
+            my_file = open("dump.html", "w")
+            my_file.write(review.get_attribute("innerHTML").encode('utf-8'))
+            my_file.close()
+
+            rating = review.find_element_by_css_selector('div:first-of-type > div:nth-child(2) > div:nth-child(1) > div > div > span > div > div').get_attribute('aria-label')
+            rating = rating.split(':')[1].split(' ')[1]
+            name = review.find_element_by_css_selector('div:first-of-type > div:nth-child(2) > div:nth-child(1) > div > span').text
+            comment = review.find_element_by_css_selector('div:first-of-type > div:nth-child(2) > div:nth-child(2) > span').text
+
+            print name + ' ('+rating+'): ' + comment
+            writer.writerow([name.encode('utf-8'),rating.encode('utf-8'),comment.encode('utf-8')])
+        except:
+            print("error")
             
 driver.quit()
+exit()
